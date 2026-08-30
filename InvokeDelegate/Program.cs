97889cfbcc48c19cecf70d3db6a70e2c30258b9b
Adrian Bailador <adrianbailador@hotmail.com>
2026-08-30 15:42:07 +0100

    Make InvokeDelegate actually demonstrate delegates
    The project was a leftover copy of IndexOfArray's array-search code
    and never invoked a delegate at all.

--- InvokeDelegate/Program.cs
+++ InvokeDelegate/Program.cs
@@ -1,67 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
 
 public class Program
 {
+    private delegate int Operation(int a, int b);
+
     public static void Main()
     {
-        const int arraySize = 100;
-        const int targetValue = 1;
+        Operation add = Add;
+        Operation subtract = Subtract;
+        Operation multiply = (a, b) => a * b;
 
-        int[] array = CreateArrayWithRandomTarget(arraySize, targetValue);
-        PrintArray(array, targetValue);
-
-        (int index, int attempts) = FindTargetIndexAndAttempts(array, targetValue);
-
-        Console.WriteLine($"\nIndex: {index}, Attempts: {attempts}");
+        Invoke(add, 5, 3);
+        Invoke(subtract, 5, 3);
+        Invoke(multiply, 5, 3);
     }
 
-    private static int[] CreateArrayWithRandomTarget(int size, int targetValue)
+    private static int Add(int a, int b) => a + b;
+
+    private static int Subtract(int a, int b) => a - b;
+
+    private static void Invoke(Operation operation, int a, int b)
     {
-        int[] array = new int[size];
-        Random random = new();
-
-        int randomIndex = random.Next(size);
-        array[randomIndex] = targetValue;
-
-        return array;
-    }
-
-    private static void PrintArray(IReadOnlyList<int> array, int targetValue)
-    {
-        foreach (int i in array)
-        {
-            if (i == targetValue)
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(targetValue);
-                Console.ResetColor();
-            }
-            else Console.Write(i);
-        }
-    }
-
-    private static (int, int) FindTargetIndexAndAttempts(IReadOnlyList<int> array, int targetValue)
-    {
-        int attempts = 0;
-        int count = array.Count;
-
-        for (int i = count / 2; i < count; i++)
-        {
-            attempts++;
-            if (array[i] == targetValue)
-            {
-                return (i, attempts);
-            }
-        }
-        for (int i = count / 2 - 1; i > 0; i--)
-        {
-            attempts++;
-            if (array[i] == targetValue)
-            {
-                return (i, attempts);
-            }
-        }
-        return (-1, attempts);
+        int result = operation(a, b);
+        Console.WriteLine($"{operation.Method.Name}({a}, {b}) = {result}");
     }
 }
